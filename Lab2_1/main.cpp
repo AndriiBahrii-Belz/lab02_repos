c6a17833ff7e9c42601113a67308f24532595f8c
AndriiBahrii-Belz <andribagribelz@gmail.com>
2023-09-18 12:50:20 +0300

    first

---
+++ Lab2_1/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <math.h>
+#define _USE_MATH_DEFINES
+
+using namespace std;
+int main()
+{
+	double x, z1, z2;
+
+	cout << "x = "; cin >> x;
+
+	double sec = 1 / cos(2*x);
+
+	z1 = (cos(x) + sin(x)) / (cos(x) - sin(x)); //1 рівняння
+	z2 = (tan(2 * x)) + sec; //2 рівняння
+
+	cout << "z1 = " << z1 << endl;
+	cout << "z2 = " << z2 << endl;
+
+	cin.get();
+	return 0;
+}
